35d86f434a985c7c6783dd0f2f257b46f2b89c3a
Dennis Hermes <78319503+DennisHermes@users.noreply.github.com>
2021-08-28 15:27:42 +0200

    * Case insensitive

---
+++ commands/help copy.js
@@ -0,0 +1,12 @@
+const {MessageEmbed} = require('discord.js');
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+    
+}
+
+module.exports.info = {
+    name: "play",
+    description: "Play some music",
+    category: "musix"
+}
